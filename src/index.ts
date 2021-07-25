0a402da4025740fc52e3c9057bfbeac8fc761a83
haruki kondo <avp.104.106.107@ae.auone-net.jp>
2021-07-25 17:03:25 +0900

    型宣言の例を追加しました。

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,14 @@
  * TypeScript用のインデックスファイル
  */
 
-console.log('Hello TypeScript!')
+console.log('Hello TypeScript!');
+
+// 型宣言の出力する。
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+};
+let d = c.apple* 4;
+console.log(d);
