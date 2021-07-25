43a0a2dd1873fab3b0346628dcffdea9399c136c
haruki kondo <avp.104.106.107@ae.auone-net.jp>
2021-07-25 21:41:52 +0900

    関数を実装する。

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,27 @@ let c = {
     apple: a,
     banana: b
 };
-let d = c.apple* 4;
+let d = c.apple　* 4;
 console.log(d);
+
+type Reservation = unknown;
+
+// 関数の実装例
+type Reserve = {
+    (from: Date, to:Due, destination: string): Reservation
+    (from: Date, destination: string): Reservation
+    (destination: string): Reservation
+}
+
+// Reserveの概要を実装する。
+let reserve: Reserve = (fromOrDestination: Date | string, to: toOrDestination Date | string, destination?: string) => {
+    // to日が指定されている場合(一日以上かかる場合)
+    if (fromOrDestination instanceof Date && toOrDestination instanceof Date && destination !== undefined) {
+        // 宿泊旅行を予約する。
+    }else if (toOrDestination instanceof Date && destination !== undefined) {
+        // 日帰り旅行を予約する。
+    } else if (typeof toOrDestination === 'string') {
+        // すぐに出発する旅行を予約する。
+    }
+}
+
