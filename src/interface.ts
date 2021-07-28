33a2156f0607a9bc6fd175c595c814f0e086a445
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-07-28 23:20:01 +0900

    パッケージ追加

---
+++ src/interface.ts
@@ -0,0 +1,38 @@
+/**
+ * TypeScriptを用いたインターフェースのデザインパターンを学ぶためのファイル
+ */
+
+// 型Shoeを定義
+type Shoe = {
+    purpose: string
+}
+
+// 具象クラス
+class BalletFlat implements Shoe {
+    purpose = 'dancing';
+}
+
+// 具象クラス2
+class Boot implements Shoe {
+    purpose = 'woodcutting';
+}
+
+// 具象クラス3
+class Sneaker implements Shoe {
+    purpose = 'walking';
+}
+
+// Shoeのファクトリー
+let Shoe = {
+    create(type: 'balletFlat' | 'boot' | 'sneaker'): Shoe {
+        // typeの値によって変化
+        switch (type) {
+            case 'balletFlat': 
+                return new BalletFlat
+            case 'boot': 
+                return new Boot
+            case 'sneaker':
+                return new Sneaker
+        }
+    }
+}
