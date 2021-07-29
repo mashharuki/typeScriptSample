a050b791f78895600f4049776f6ebd1aafc8b7ec
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-07-29 20:56:35 +0900

    安全な設計方針に修正

--- src/interface2.ts
+++ src/interface2.ts
@@ -5,20 +5,13 @@
 // RequestBuilderクラス
 class RequestBuilder {
     // プライベート変数
-    private url: string | null = null;
-    private method: 'get' | 'post' | null = null;
-    private data: object | null = null;
-
-    // URLのsetterメソッド
-    setURL(url: string): this {
-        this.url = url
-        return this
-    }
+    protected url: string | null = null;
+    protected method: 'get' | 'post' | null = null;
+    protected data: object | null = null;
 
     // methodのsetterメソッド
-    setMethod(method: 'get' | 'post'): this {
-        this.method = method
-        return this
+    setMethod(method: 'get' | 'post'): RequestBuilderWithMethod {
+        return new RequestBuilderWithMethod().setMethod(method).setData(this.data)
     }
 
     // dataのsetterメソッド
@@ -27,3 +20,34 @@ class RequestBuilder {
         return this
     }
 }
+
+// RequestBuilderWithMethodクラス
+class RequestBuilderWithMethod extends RequestBuilder {
+    // methodのsetterメソッド
+    setMethod(method: 'get' | 'post' | null): this {
+        this.method = method;
+        return this
+    }
+
+    // URLのsetterメソッド
+    setURL(url: string): RequestBuilderWithMethodAndURL {
+        return new RequestBuilderWithMethodAndURL().setMethod(this.method).setURL(url).setData(this.data)
+    }
+}
+
+// RequestBuilderWithMethodAndURLクラス
+class RequestBuilderWithMethodAndURL extends RequestBuilderWithMethod {
+    // setURL
+    setURL(url: string): this {
+        this.url = url;
+        return this
+    }
+
+    // send
+    send() {
+
+    }
+}
+
+// RequetsBuilderクラスのインスタンスを生成
+new RequestBuilder().setMethod('get').setData({}).setURL('foo.com').send();
